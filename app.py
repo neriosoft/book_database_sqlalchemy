99f8dc37d3769591521690b001a8ee98ea994c7b
neriosoft <alabaneri@gmail.com>
2023-01-29 19:02:54 -0500

    able to view a book

--- app.py
+++ app.py
@@ -65,8 +65,25 @@ def app():
 
         elif choice == '3':
             #search book
-            pass
+            id_option = []
+            for book in session.query(Book):
+                id_option.append(book.id)
 
+            id_error = True
+            while id_error:
+                id_choice = input(f'''
+                  \nID options: {id_option} 
+                  \rBook ID:''')
+
+                id_choice = clean_id(id_choice, id_option)
+                if type(id_choice) == int:
+                    id_error = False
+            the_book = session.query(Book).filter(Book.id==id_choice).first()
+            print(f'''
+            \n{the_book.tile} by {the_book.author}
+            \rPublished: {the_book.published_date}
+            \rPrice: ${the_book.price / 100}''')
+            input('\nPress enter to return to the main menu')
         elif choice == '4':
             #book analysis
             pass
@@ -112,6 +129,29 @@ def clean_price(price_str):
 
     else:
         return int(price_float * 100)
+
+def clean_id(id_str, options):
+    try: 
+        book_id = int(id_str)
+    
+    except ValueError:
+        input('''
+        \n********* ID Error ***********
+        \rThe ID should be a number.
+        \rPress ENTER to try again
+        *********************************''')
+        return
+    else:
+        if book_id in options:
+            return book_id
+        else:
+            input(f'''
+             \n********* ID Error ***********
+             \rOption {options}.
+             \rPress ENTER to try again
+        *********************************''')
+            return
+
 
 def add_csv():
     with open('suggested_book.csv') as csvfile:
